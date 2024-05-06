aeabf415bf7104a3ca872e247b989da7a058ac76
Krewoe <2192629378@qq.com>
2024-05-06 18:55:42 +0800

    marco

--- Lars_reactor/include/qc.hpp
+++ Lars_reactor/include/qc.hpp
@@ -2,9 +2,10 @@
 #define __QC_HPP__
 
 #include <assert.h>
-#include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
+
+#include <iostream>
 
 namespace qc {
 
@@ -18,7 +19,20 @@ typedef unsigned long long ull;
             std::abort();                                                    \
         }                                                                    \
     } while (0)
- 
+
+/**
+ * @details 编译器优化  qc_likely(x) -> x 大概率成立(1),优化; 
+ *                    qc_unlikely(x)-> x大概率不成立(0),优化;
+ *   __builtin_expect 是 GCC 和 LLVM 的内建函数，用于提供分支预测的信息
+ */
+#if         defined __GNUC__ || defined __llvm__
+#define     qc_likely(x) __builtin_expect(!!(x), 1)
+#define     qc_unlikely(x) __builtin_expect(!!(x), 0)
+#else
+#define     qc_likely(x) (x)
+#define     qc_unlikely(x) (x)
+#endif
+
 }  // namespace qc
 
 #endif
