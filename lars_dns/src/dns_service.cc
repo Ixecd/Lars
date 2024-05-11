d1ad2b3706277a2a93a08d45a69493c1fbd445f0
Krewoe <2192629378@qq.com>
2024-05-11 19:52:02 +0800

    lars_dns

--- lars_dns/src/dns_service.cc
+++ lars_dns/src/dns_service.cc
@@ -1,1 +1,21 @@
-#include "qc.hpp"
+#include "lars_reactor.hpp"
+
+using namespace qc;
+
+int main(int argc) {
+
+    event_loop loop;
+
+    // 加载配置信息
+    config_file::setPath("../conf/lars.conf");
+    std::string ip = config_file::GetInstance()->GetString("reactor", "ip", "0.0.0.0");
+    short port = config_file::GetInstance()->GetNumber("reactor", "port", 9876);
+
+    tcp_server *server = new tcp_server(&loop, ip.c_str(), port);
+
+    // 注册路由信息
+    printf("lars dns service ...\n");
+    loop.event_process();
+
+    return 0;
+}
