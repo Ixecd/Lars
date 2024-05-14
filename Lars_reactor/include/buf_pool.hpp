62b57953736509532ffe6c0f476571eaac9e078b
Krewoe <2192629378@qq.com>
2024-05-14 17:11:31 +0800

    优化

--- Lars_reactor/include/buf_pool.hpp
+++ Lars_reactor/include/buf_pool.hpp
@@ -1,7 +1,7 @@
 /**
  * @file buf_pool.hpp
  * @author qc
- * @brief
+ * @brief 方法只有开辟一块内存和回收一块内存
  * @version 0.1
  * @date 2024-04-26
  *
@@ -15,15 +15,17 @@
 #include <thread>
 #include <unordered_map>
 
+#include "mutex.hpp"
 #include "io_buf.hpp"
 #include "singleton.hpp"
 
-// 总内存池最大限制 Kb 5GB
+// 总内存池最大限制 单位Kb 5GB
 #define MAX_MEM_LIMIT (5U * 1024 * 1024)
 
 namespace qc {
 /// @brief 每个key下面挂一个io_buf链表,会预先开辟好一定数量的内存块,上层取一块,就从这里头去掉一块,用完再放回来
-typedef std::unordered_map<int, io_buf *> pool_t;
+// typedef std::unordered_map<int, io_buf *> pool_t;
+using pool_t = std::unordered_map<int, io_buf*>;
 // typedef Singleton<buf_pool> _instance;
 
 enum MEM_CAP {
@@ -38,18 +40,22 @@ enum MEM_CAP {
 /**
  * @brief 单例模式
  */
-class buf_pool {
+class buf_pool : public Singleton<buf_pool> {
+friend Singleton<buf_pool>;
 public:
-    // 初始化单例对象
-    static void init() {
-        _instance = new buf_pool();
-    }
-    // 获取单例方法
-    static buf_pool *instance() {
-        // 保证init在整个进程中只被执行一次
-        pthread_once(&_once, init);
-        return _instance;
-    }
+
+    // // 初始化单例对象
+    // static void init() {
+    //     _instance = new buf_pool();
+    // }
+    // // 获取单例方法
+    // static buf_pool *instance() {
+    //     // 保证init在整个进程中只被执行一次
+    //     pthread_once(&_once, init);
+    //     return _instance;
+    // }
+    typedef Mutex MutexType;
+
     /// @brief 开辟一个io_buf
     /// @param size
     /// @return 
@@ -73,11 +79,10 @@ private:
     /// @brief 总buffer池的内存大小,单位为KB
     uint64_t _total_mem;
     /// @brief 单例模式
-    static buf_pool *_instance;
-    /// @brief 用于保证创建单例的init方法只执行一次的锁
-    static pthread_once_t _once;
+    /// buf_pool::GetInstance()
     /// @brief 用于保存内存池链表修改的互斥锁
-    static pthread_mutex_t _mutex;
+    // static pthread_mutex_t _mutex;
+    MutexType _mutex;
 };
 
 }  // namespace qc
