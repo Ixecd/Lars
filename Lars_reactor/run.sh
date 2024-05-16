d84d8c0872a1fc780fb6238a9ab8d434e9ec429a
Krewoe <2192629378@qq.com>
2024-05-16 21:08:47 +0800

    脚本

--- Lars_reactor/run.sh
+++ Lars_reactor/run.sh
@@ -2,9 +2,11 @@ make clean
 make 
 
 # cd example/lars_reactor_0.5
-cd  example/lars_reactor_1.0/
-make clean
-make
+# cd  example/lars_reactor_1.0/
+# make clean
+# make
 
-cd ../../src
-rm *.o
+# cd ../../src
+# rm *.o
+cd src
+rm -rf *.o
