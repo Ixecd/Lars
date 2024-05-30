fab7c1d92ce87a412ba7113073bb2678d2c83db0
Krewoe <2192629378@qq.com>
2024-05-30 21:32:34 +0800

    api

---
+++ api/cpp/example/example.cc
@@ -0,0 +1,29 @@
+#include "lars_api.hpp"
+#include <iostream>
+
+using namespace qc;
+
+void usage() {
+    std::cout << "usage: ./example [modid] [cmdid]\n";
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        usage();
+        return 1;
+    }
+
+    int modid = (int)argv[1];
+    int cmdid = (int)argv[2];
+
+    std::string ip;
+    int port;
+
+    lars_client api;
+    int rt = api.get_host(modid, cmdid, ip, port);
+    if (rt == 0) {
+        std::cout << "host is " << ip << " : " << port << std::endl;
+        // TODO 上报调用结果
+    }
+    return 0;
+}
