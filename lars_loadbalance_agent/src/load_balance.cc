20ba101b0a46dc2b067b5ef1b44ef25899c7ee11
Krewoe <2192629378@qq.com>
2024-06-01 18:13:37 +0800

    report具体实现

--- lars_loadbalance_agent/src/load_balance.cc
+++ lars_loadbalance_agent/src/load_balance.cc
@@ -1,10 +1,13 @@
 #include "load_balance.hpp"
+
 #include "main_server.hpp"
 #define PROBE_NUM 10
 #define INIT_SUCC_CNT 128
 
 // 与dns_client通信的thread_queue消息队列
 extern qc::thread_queue<lars::GetRouteRequest> *dns_queue;
+// 与report_client通信的消息队列
+extern qc::thread_queue<lars::ReportStatusReq> *report_queue;
 
 namespace qc {
 
@@ -60,7 +63,6 @@ int load_balance::choice_one_host(lars::GetHostResponse &rsp) {
             // 目前不给机会
             get_host_from_list(rsp, _idle_list);
         }
-
     }
     return lars::RET_SUCC;
 }
@@ -78,7 +80,7 @@ int load_balance::pull() {
 }
 
 /// @brief 向自己的idle_list中添加主机信息
-/// @param rsp 
+/// @param rsp
 void load_balance::update(lars::GetRouteResponse &rsp) {
     qc_assert(rsp.host_size() != 0);
 
@@ -88,8 +90,8 @@ void load_balance::update(lars::GetRouteResponse &rsp) {
     for (int i = 0; i < rsp.host_size(); ++i) {
         // get cur host
         const lars::HostInfo &HI = rsp.host(i);
-        
-        uint64_t key = ((uint64_t)HI.ip() << 32) + HI.port(); 
+
+        uint64_t key = ((uint64_t)HI.ip() << 32) + HI.port();
 
         remote_hosts.insert(key);
 
@@ -105,7 +107,7 @@ void load_balance::update(lars::GetRouteResponse &rsp) {
 
     // 得到的所有结点都要删除
     for (auto it = _host_map.begin(); it != _host_map.end(); ++it) {
-        if (remote_hosts.find(it->first) == remote_hosts.end()) 
+        if (remote_hosts.find(it->first) == remote_hosts.end())
             need_delete.insert(it->first);
     }
 
@@ -113,13 +115,139 @@ void load_balance::update(lars::GetRouteResponse &rsp) {
     for (auto it = need_delete.begin(); it != need_delete.end(); ++it) {
         uint64_t key = *it;
         host_info *hi = _host_map[key];
-        if (hi->overload) 
+        if (hi->overload)
             _overload_list.remove(hi);
-        else _idle_list.remove(hi);
+        else
+            _idle_list.remove(hi);
         delete hi;
     }
-    
-
 }
 
+/// @brief 上报当前host主机调用情况给远端repoter service
+void load_balance::report(int ip, int port, int retcode) {
+    uint64_t key = ((uint64_t)ip << 32) + port;
+
+    if (_host_map.find(key) == _host_map.end()) {
+        return;
+    }
+
+    // 更新
+
+    // 1 计数统计
+    host_info *hi = _host_map[key];
+    if (retcode == lars::RET_SUCC) {
+        // 更新虚拟成功,真实成功
+        hi->vsucc++;
+        hi->rsucc++;
+
+        hi->contin_succ++;
+        hi->contin_err = 0;
+    } else {
+        // 更新虚拟失败,真实失败
+        hi->verr++;
+        hi->rerr++;
+
+        hi->contin_err++;
+        hi->contin_succ = 0;
+    }
+
+    // 2.检查节点状态
+    /// @details 关于idle节点只有返回值不为SUCC才去判断是否更新为overload状态
+    if (hi->overload == false && retcode != lars::RET_SUCC) {
+        bool overload = false;
+
+        double err_rate = hi->verr * 1.0 / (hi->verr + hi->vsucc);
+
+        if (err_rate > lb_config.err_rate) overload = true;
+
+        if (overload == false &&
+            hi->contin_err >= (uint32_t)lb_config.contin_err_limits)
+            overload = true;
+
+        if (overload == true) {
+            // 输出一下过载信息
+            struct in_addr saddr;
+            saddr.s_addr = htonl(hi->ip);
+
+            printf("[%d, %d] host %s : %d change overload, succ %u, err %u\n",
+                   _modid, _cmdid, inet_ntoa(saddr), hi->port, hi->vsucc,
+                   hi->verr);
+
+            hi->set_overload();
+
+            _idle_list.remove(hi);
+            _overload_list.push_back(hi);
+            return;
+        }
+    }  // 如果当前节点的状态是overload那只有调用成功才会有可能重新设置为idle
+    else if (hi->overload == true && retcode == lars::RET_SUCC) {
+        bool idle = false;
+
+        double succ_rate = hi->vsucc * 1.0 / (hi->vsucc + hi->verr);
+        if (succ_rate >= lb_config.succ_rate) idle = true;
+
+        if (hi->contin_succ >= (uint32_t)lb_config.contin_succ_limits)
+            idle = true;
+
+        if (idle == true) {
+            // 输出一下信息
+            struct in_addr saddr;
+            saddr.s_addr = htonl(hi->ip);
+
+            printf("[%d, %d] host %s : %d change idle, succ %u, err %u\n",
+                   _modid, _cmdid, inet_ntoa(saddr), hi->port, hi->vsucc,
+                   hi->verr);
+
+            hi->set_idle();
+
+            // 更改列表
+            _overload_list.remove(hi);
+            _idle_list.push_back(hi);
+            return;
+        }
+    }
+    // TODO 窗口检查和超时机制
 }
+
+// 上报结果
+void load_balance::commit() {
+    if (this->empty()) return;
+    // 1.封装请求消息
+    lars::ReportStatusReq req;
+    req.set_modid(_modid);
+    req.set_cmdid(_cmdid);
+    req.set_caller(lb_config.local_ip);
+    req.set_ts(time(nullptr));
+
+    // 2. 从idle_list取值
+    for (host_list_it it = _idle_list.begin(); it != _idle_list.end(); it++) {
+        host_info *hi = *it;
+        lars::HostCallResult call_res;
+        call_res.set_ip(hi->ip);
+        call_res.set_port(hi->port);
+        call_res.set_succ(hi->rsucc);
+        call_res.set_err(hi->rerr);
+        call_res.set_overload(false);
+
+        req.add_results()->CopyFrom(call_res);
+    }
+
+    // 3. 从over_list取值
+    for (host_list_it it = _overload_list.begin(); it != _overload_list.end();
+         it++) {
+        host_info *hi = *it;
+        lars::HostCallResult call_res;
+        call_res.set_ip(hi->ip);
+        call_res.set_port(hi->port);
+        call_res.set_succ(hi->rsucc);
+        call_res.set_err(hi->rerr);
+        call_res.set_overload(true);
+
+        req.add_results()->CopyFrom(call_res);
+    }
+
+    // 4 发送给report_client 的消息队列
+    report_queue->send(req);
+}
+
+}  // namespace qc
