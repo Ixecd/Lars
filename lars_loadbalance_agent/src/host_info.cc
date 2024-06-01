4a34459d66e1cd751b8aee3a1d254fabe92746c3
Krewoe <2192629378@qq.com>
2024-06-01 18:12:55 +0800

    添加主机信息

---
+++ lars_loadbalance_agent/src/host_info.cc
@@ -0,0 +1,40 @@
+/**
+ * @file host_info.cc
+ * @author qc
+ * @brief host_info信息
+ * @version 0.1
+ * @date 2024-06-01
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "host_info.hpp"
+
+#include "main_server.hpp"
+
+namespace qc {
+
+void host_info::set_overload() {
+    vsucc           = 0;
+    verr            = lb_config.init_err_cnt;
+    vsucc           = 0;
+    rsucc           = 0;
+    rerr            = 0;
+    contin_succ     = 0;
+    contin_err      = 0;
+    overload        = true;
+}
+
+void host_info::set_idle() {
+    vsucc           = lb_config.init_succ_cnt;
+    verr            = 0;
+    vsucc           = 0;
+    rsucc           = 0;
+    rerr            = 0;
+    contin_succ     = 0;
+    contin_err      = 0;
+    overload        = false;
+}
+
+}  // namespace qc
