9d4006354ca178ff28f948f23dd3a0f984c1e725
Krewoe <2192629378@qq.com>
2024-06-01 18:09:19 +0800

    设置了load_balance_config

--- lars_loadbalance_agent/include/main_server.hpp
+++ lars_loadbalance_agent/include/main_server.hpp
@@ -10,10 +10,33 @@
  */
 
 #pragma once
+#include <netdb.h>
 #include "lars_reactor.hpp"
 #include "lars.pb.h"
 
+struct load_balance_config {
+
+    int probe_num;
+
+    int init_succ_cnt;
+
+    int init_err_cnt;
+
+    float err_rate;
+
+    float succ_rate;
+
+    int contin_err_limits;
+
+    int contin_succ_limits;
+    // 当前agent本地ip地址(上报 填充caller字段)
+    uint32_t local_ip;
+};
+// 在头文件中声明一个变量,之后都用这个
+struct load_balance_config lb_config;
+
 namespace qc {
+
     // 启动UDP server,用来接收业务层(调用者/使用者)的消息
     void start_UDP_servers();
 
@@ -23,9 +46,5 @@ namespace qc {
     // 启动lars_dns client线程
     void start_dns_client();
 
-    
 }
-
-
-
 
