4fcd6977e4e2fa47585ab2e44794313e210b6d6b
Ixecd <2192629378@qq.com>
2024-08-20 17:59:43 +0800

    RAII

--- lars_reactor/src/thread_pool.cc
+++ lars_reactor/src/thread_pool.cc
@@ -1,10 +1,15 @@
 #include <unistd.h>
+#include <memory>
 #include <lars_reactor/thread_pool.hpp>
 #include <lars_reactor/event_loop.hpp>
 #include <lars_reactor/tcp_conn.hpp>
 
-namespace qc {
+// 考虑到RAII
+// 规定只在创建的时候make_shared一次,后面都用get,裸指针
+// 因为一般没人手动delete,是为了防止内存泄漏,并且同时兼顾了内存开销,同时还保留了Cpp指针特色
+// 良好的Cpp程序是不能出现 new 和 delete 的
 
+namespace qc {
 /**
  * @brief 一旦有task任务过来,就会执行这个函数,这个函数时处理task消息业务的主流程
  *        只要有人调用thread_queue::send()方法就会触发这个函数
@@ -23,7 +28,8 @@ void deal_task_message(event_loop *loop, int fd, void *args) {
         tasks.pop();
 
         if (task.type == task_msg::NEW_CONN) {
-            tcp_conn *conn = new tcp_conn(task.connfd, loop);
+            std::shared_ptr<tcp_conn> conn = std::make_shared<tcp_conn>(task.connfd, loop);
+            // tcp_conn *conn = new tcp_conn(task.connfd, loop);
             qc_assert(conn != nullptr);
             printf("[thread] : get new connection succ!\n");
         } else if (task.type == task_msg::NEW_TASK) {
@@ -41,15 +47,16 @@ void *thread_main(void *args) {
     thread_queue<task_msg> *queue = (thread_queue<task_msg> *)args;
 
     // 每个线程都应该有一个event_loop来监控客户端连接的读写事件
-    event_loop *loop = new event_loop();
+    // event_loop *loop = new event_loop();
+    std::shared_ptr<event_loop> loop = std::make_shared<event_loop>();
+    // 不推荐下面的写法,相比于上面要多申请一次内存
+    // ! std::shared_ptr<event_loop> loop(new event_loop());
     qc_assert(loop != nullptr);
 
-    queue->set_loop(loop);
+    queue->set_loop(loop.get());
     // 后面的参数时任务队列
     queue->set_callback(deal_task_message, queue);
-
     loop->event_process();
-
     return nullptr;
 }
 
@@ -64,7 +71,7 @@ void thread_pool::send_task(task_func func, void *args) {
         task.args = args;
         // 一个thread对应一个消息队列,这里由于要发送给所有线程
         // 要拿到所有消息队列
-        thread_queue<task_msg> *queue = _queues[i];
+        thread_queue<task_msg> *queue = _queues[i].get();
         queue->send(task);
     }
 }
@@ -72,13 +79,14 @@ void thread_pool::send_task(task_func func, void *args) {
 thread_pool::thread_pool(size_t threads) {
     qc_assert(threads > 0);
     _index = 0;
-    _queues = nullptr;
+    _queues.resize(threads);
     _thread_cnt = threads;
 
     // 任务队列的个数要和线程池中线程的数量一致
     // printf("cur threads = %d \n", threads);
-    _queues = new thread_queue<task_msg>*[threads];
-    _tids = new pthread_t[threads];
+    
+    // _queues = new thread_queue<task_msg>*[threads];
+    // _tids = new pthread_t[threads];
 
     int rt;
     for (size_t i = 0; i < threads; ++i) {
@@ -86,10 +94,10 @@ thread_pool::thread_pool(size_t threads) {
         printf("create %ld thread\n", i);
         // 给当前线程创建一个任务消息队列
         // 一个thread_queue<task_msg>中单独有一个evfd和loop
-        _queues[i] = new thread_queue<task_msg>();
+        _queues[i] = std::make_shared<thread_queue<task_msg>>();
         qc_assert(_queues[i] != nullptr);
 
-        rt = pthread_create(&_tids[i], nullptr, thread_main, _queues[i]);
+        rt = pthread_create(&_tids[i], nullptr, thread_main, _queues[i].get());
         qc_assert(rt == 0);
 
         // 线程脱离
@@ -101,7 +109,7 @@ thread_queue<task_msg>* thread_pool::get_thread() {
 	// 这里修改为_index轮询发送消息
     if (_index == _thread_cnt) _index = 0;
     else _index++;
-    return _queues[_index];
+    return _queues[_index].get();
 }
 
 }
